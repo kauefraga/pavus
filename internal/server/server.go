ee3999bb88047c7a74e89b4a602c238f4319a82d
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-18 00:27:54 -0300

    feat: serve static files and set the page icon

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,8 +13,11 @@ import (
 // TODO: shutdown gracefully
 // TODO: add styles (css)
 
-//go:embed previewer/layout.html
+//go:embed static/layout.html
 var layout embed.FS
+
+//go:embed static/*
+var assets embed.FS
 
 type LayoutData struct {
 	Content template.HTML
@@ -22,8 +25,9 @@ type LayoutData struct {
 
 func ServeAndWatch(mdPath string) {
 	http.HandleFunc("/ws", newWebSocketHandler(mdPath))
+	http.Handle("/static/", http.FileServer(http.FS(assets)))
 
-	tmpl, err := template.ParseFS(layout, "previewer/layout.html")
+	tmpl, err := template.ParseFS(layout, "static/layout.html")
 	if err != nil {
 		fmt.Println("Error: failed to parse the layout template file")
 		os.Exit(1)
