f8b7990ee15f887341a4e4a840a25a66c748e4b8
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-17 23:09:21 -0300

    chore: create a build script and ignore its output

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+echo "building pavus for the following platforms: windows, linux and darwin"
+
+if [ ! -d "bin" ]; then
+  mkdir bin
+fi
+
+CGO_ENABLED=0 GOOS=windows go build -ldflags='-w -s' -o bin/pavus.exe cmd/main.go
+CGO_ENABLED=0 GOOS=linux go build -ldflags='-w -s' -o bin/pavus cmd/main.go
+CGO_ENABLED=0 GOOS=darwin go build -ldflags='-w -s' -o bin/pavus-darwin cmd/main.go
