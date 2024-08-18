2ec20dcf4aa38279d161ab56a466c43915407fd7
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-18 16:15:16 -0300

    feat: specify version

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -13,6 +13,7 @@ func getRootCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "pavus",
 		Short:   "The next-gen markdown tool",
+		Version: "1.0.0",
 		Example: "pavus\npavus path/to/markdown.md",
 		Args:    cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
