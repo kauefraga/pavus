dab36aa2cb7d411fb3cb3f12c0937848f6b6dcba
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-18 10:40:39 -0300

    chore: add a finish message

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ fi
 CGO_ENABLED=0 GOOS=windows go build -ldflags='-w -s' -o bin/pavus.exe cmd/main.go
 CGO_ENABLED=0 GOOS=linux go build -ldflags='-w -s' -o bin/pavus cmd/main.go
 CGO_ENABLED=0 GOOS=darwin go build -ldflags='-w -s' -o bin/pavus-darwin cmd/main.go
+
+echo "finished, look in the bin/ folder"
