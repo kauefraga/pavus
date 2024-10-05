11d2d0b6a7fb85cacdd3d04621bed4682f508c1a
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-10-05 16:21:56 -0300

    chore: update main.go directory

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ if [ ! -d "bin" ]; then
   mkdir bin
 fi
 
-CGO_ENABLED=0 GOOS=windows go build -ldflags='-w -s' -o bin/pavus.exe cmd/pavus/main.go
-CGO_ENABLED=0 GOOS=linux go build -ldflags='-w -s' -o bin/pavus cmd/pavus/main.go
-CGO_ENABLED=0 GOOS=darwin go build -ldflags='-w -s' -o bin/pavus-darwin cmd/pavus/main.go
+CGO_ENABLED=0 GOOS=windows go build -ldflags='-w -s' -o bin/pavus.exe cmd/main.go
+CGO_ENABLED=0 GOOS=linux go build -ldflags='-w -s' -o bin/pavus cmd/main.go
+CGO_ENABLED=0 GOOS=darwin go build -ldflags='-w -s' -o bin/pavus-darwin cmd/main.go
 
 echo "finished, look in the bin/ folder"
